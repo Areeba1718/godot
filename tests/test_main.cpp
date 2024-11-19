ebfb371d268ae38a4bfab0b4f18f9a75046944e9
Sequoia Haynes <supersequoia11@gmail.com>
2024-11-19 10:50:09 -0800

    Oops, accidentally removed a test from test_main

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -119,6 +119,7 @@
 #include "tests/scene/test_gradient.h"
 #include "tests/scene/test_gradient_texture.h"
 #include "tests/scene/test_image_texture.h"
+#include "tests/scene/test_image_texture_3d.h"
 #include "tests/scene/test_occluder_3d.h"
 #include "tests/scene/test_instance_placeholder.h"
 #include "tests/scene/test_node.h"
